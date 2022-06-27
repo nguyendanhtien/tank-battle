79cfeff2f4514f0c614b052b19a57068da9b542c
tungnthust <thanhtungbka1@gmail.com>
2022-06-27 23:47:18 +0700

    update server

--- server/client.c
+++ server/client.c
@@ -14,57 +14,16 @@ void *send_msg_handler(void *sockfd);
 void *recv_msg_handler(void *sockfd);
 volatile sig_atomic_t flag = 0;
 
-void ctrl_c_handler(int sig) {
-    flag = 1;
-}
-void recv_msg(int sockfd, char* msg)
-{
-    int n = read(sockfd, msg, 3);
-    
-    if (n < 0 || n != 3)
-     error("ERROR reading message from server socket.");
-
-    printf("[DEBUG] Received message: %s\n", msg);
-   }
-
-int recv_int(int sockfd)
-{
-    int msg = 0;
-    int n = read(sockfd, &msg, sizeof(int));
-    
-    if (n < 0 || n != sizeof(int)) 
-        error("ERROR reading int from server socket");
-    
-    printf("[DEBUG] Received int: %d\n", msg);
-    
-    return msg;
-}
-
-void write_server_int(int sockfd, int msg)
-{
-    int n = write(sockfd, &msg, sizeof(int));
-    if (n < 0)
-        error("ERROR writing int to server socket");
-    
-    printf("[DEBUG] Wrote int to server: %d\n", msg);
-}
 
 void send_msg(int sockfd, char* msg)
 {
     int n = send(sockfd, msg, sizeof(msg), 0);;
     if (n < 0)
-        error("ERROR sending message to server socket");
+        printf("ERROR sending message to server socket");
     
     printf("[DEBUG] Sending message to server: %s\n", msg);
 }
 
-void error(const char *msg)
-{
-    perror(msg);
-    printf("Either the server shut down or the other player disconnected.\nGame over.\n");
-    
-    exit(0);
-}
 int connect_to_server(char * hostname, int portno)
 {
     struct sockaddr_in serv_addr;
@@ -73,7 +32,7 @@ int connect_to_server(char * hostname, int portno)
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	
     if (sockfd < 0) 
-        error("ERROR opening socket for server.");
+        printf("ERROR opening socket for server.");
 	
     server = gethostbyname(hostname);
 	
@@ -89,7 +48,7 @@ int connect_to_server(char * hostname, int portno)
     serv_addr.sin_port = htons(portno); 
 
    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR connecting to server");
+        printf("ERROR connecting to server");
 
     printf("[DEBUG] Connected to server.\n");
      return sockfd;
@@ -144,9 +103,8 @@ void *send_msg_handler(void *sockfd) {
 	int read_len;
     char msg[MAX_MSG_LEN];
 	while (1) {
-        printf("Enter message: ");
+        // printf("Enter message: ");
         fgets(msg, MAX_MSG_LEN, stdin);
-        printf("%s", msg);
         send(socket, msg, strlen(msg), 0);
         if (!strcmp(msg, "exit"))
             break;
@@ -164,12 +122,12 @@ void *recv_msg_handler(void *sockfd) {
     char msg[MAX_MSG_LEN];
     memset(msg, 0, MAX_MSG_LEN);
 	while ((msg_len = recv(socket, msg, MAX_MSG_LEN, 0)) > 0) {
-        char msg_type[4];
+        char msg_type[5];
         msg[msg_len] = '\0';
-        strncpy(msg_type, &msg[0], 3);
-        msg_type[3] = '\0';
-        if (!strcmp(msg_type, "MAT")) {
-            char send_msg[MAX_MSG_LEN] = "STA";
+        strncpy(msg_type, &msg[0], 4);
+        msg_type[4] = '\0';
+        if (!strcmp(msg_type, "MATC")) {
+            char send_msg[MAX_MSG_LEN] = "PLAY";
             send(socket, send_msg, strlen(send_msg), 0);
             bzero(send_msg, MAX_MSG_LEN);
         }
--- server/server.c
+++ server/server.c
@@ -13,6 +13,16 @@
 
 #define MAX_MSG_LEN 64
 #define USER_CAPACITY 256
+#define MAX_GAME_TIME 180
+#define MAX_POWER_TIME 10
+struct PlayerStruct {
+    int coord[3];
+    int hp;
+    int is_shot;
+    int power_elapsed;
+};
+
+typedef struct PlayerStruct Player;
 
 int setup_listener(int portno);
 int matching_request_handler(char *message, int msg_len, int socket);
@@ -20,7 +30,9 @@ void *client_connection_handler(void *cli_sockfd);
 void send_client_msg(int cli_sockfd, char * msg);
 void send_clients_msg(int cli_sockfd1, int cli_sockfd2, char * msg);
 void *run_game(void *room_info);
-
+Player* player_init();
+void reset_stats(Player* p1, Player* p2);
+int max(int a, int b);
 int player_count = 0;
 int game_count = 0;
 
@@ -68,7 +80,7 @@ int main(int argc, char *argv[]) {
 
             pthread_mutex_lock(&mutexcount);
             player_count++;
-            printf("Number of players is now %d.\n", player_count);
+            printf("[DEBUG]Number of players is now %d.\n", player_count);
             
             
             // pthread_t client_thread;
@@ -101,14 +113,43 @@ int main(int argc, char *argv[]) {
     pthread_exit(NULL);
 }
 
+
+Player* player_init(){
+    struct PlayerStruct* temp = (struct PlayerStruct*)malloc(sizeof(struct PlayerStruct));
+    temp->hp = 10;
+    temp->coord[0] = 0;
+    temp->coord[1] = 0;
+    temp->coord[2] = 0;
+    temp->is_shot = 0;
+    temp->power_elapsed = 0;
+    return temp;
+}
+
+void reset_stats(Player* p1, Player* p2) {
+    p1->is_shot = 0;
+    p2->is_shot = 0;
+}
+
+int max(int a, int b) {
+    if (a >= b) 
+        return a;
+    else 
+        return b;
+}
+int calc_time(time_t begin) {
+    time_t end = time(0);
+    // printf("Time spend: %ld %ld", end, end-begin);
+    return (int) (end - begin);
+    // return (int)time_spent;
+}
 void *run_game(void *room_info) {
     
     struct Room room = *(struct Room*) room_info;
     int client1 = room.client1;
     int client2 = room.client2;
     
-    int h1 = 5;
-    int h2 = 5;
+    Player* p1 = player_init();
+    Player* p2 = player_init();
     
     
     printf("---TANK BATTLE---\n");
@@ -123,9 +164,20 @@ void *run_game(void *room_info) {
     
     int poll_ret;
     int msg_len;
+    char items_state[6] = "11111";
+    int time_elapsed = MAX_GAME_TIME;
     char buff[MAX_MSG_LEN];
     bzero(buff, MAX_MSG_LEN);
     int gameover = 0;
+    time_t begin, power_clock1, power_clock2;
+     
+    begin = time(0);
+    power_clock1 = time(0);
+    power_clock2 = time(0);
+    // printf("TIME INIT: %ld %ld %ld", begin, power_clock1, power_clock2);
+    /* here, do your time-consuming job */
+
+    int power_elapsed;
     while (!gameover) {
 
 		poll_ret = poll(pfds, 2, 500);
@@ -138,11 +190,30 @@ void *run_game(void *room_info) {
 				for(int i = 0; i < 2; i++ )	{
 
 					if( pfds[i].revents & POLLIN ) {
-						
+                        // clock_t end = clock();
+                        time_elapsed = 180 - calc_time(begin);
+
+                        if (time_elapsed <= 0) {
+                            if (p1->hp <= p2->hp) {
+                                send_client_msg(pfds[1].fd, "ENDS:WON");
+                                send_client_msg(pfds[0].fd, "ENDS:LOSE");
+                                gameover = 1;
+                            } else {
+                                send_client_msg(pfds[0].fd, "ENDS:WON");
+                                send_client_msg(pfds[1].fd, "ENDS:LOSE");
+                                gameover = 1;
+                            }
+                        }
+						if ((power_elapsed = calc_time(power_clock1)) > 6) {
+                            p1->power_elapsed = max(MAX_POWER_TIME - power_elapsed, 0);
+                        }
+                        if ((power_elapsed = calc_time(power_clock2)) > 6) {
+                            p2->power_elapsed = max(MAX_POWER_TIME - power_elapsed, 0);
+                        }
 						msg_len = recv(pfds[i].fd, buff, MAX_MSG_LEN, 0);
 
                         if (msg_len <= 0) {
-                            printf("Player %d disconnected.\n", pfds[i].fd);
+                            printf("[DEBUG]Player %d disconnected.\n", pfds[i].fd);
                             gameover = 1;
                             break;
                         }
@@ -150,33 +221,103 @@ void *run_game(void *room_info) {
                         buff[msg_len] = '\0';
                         printf("[IN-GAME] Client %d: %s\n", pfds[i].fd, buff);
 
-                        char msg_type[4];
+                        char msg_type[5];
 
-                        strncpy(msg_type, &buff[0], 3);
-                        msg_type[3] = '\0';
-                        if (!strcmp(msg_type, "ATT")) {
-                            if (i == 0) {
-                                h2--;
-                            } else {
-                                h1--;
+                        strncpy(msg_type, &buff[0], 4);
+                        msg_type[4] = '\0';
+
+                        if (!strcmp(msg_type, "MOVE")) {
+                            char coords_str[msg_len-4];
+                            strncpy(coords_str, &buff[5], msg_len-5);
+                            coords_str[msg_len-4] = '\0';
+                            char * token = strtok(coords_str, "-");
+                            // loop through the string to extract all other tokens
+                            int j = 0;
+                            while( token != NULL ) {
+                                if (i == 0) {
+                                    p1->coord[j] = atoi(token);
+                                } else {
+                                    p2->coord[j] = atoi(token);
+                                }
+                                token = strtok(NULL, "-");
+                                j++;
                             }
                         }
+
+                        if (!strcmp(msg_type, "GHIT")) {
+                            if (i == 0) {
+                                if (p2->power_elapsed)
+                                    p1->hp -= 3;
+                                else
+                                    p1->hp--;
+                            } else {
+                                if (p1->power_elapsed)
+                                    p2->hp -= 3;
+                                else
+                                    p2->hp--;
+                            }
+                        }
+
+                        if (!strcmp(msg_type, "SHOT")) {
+                            if (i == 0) {
+                                p1->is_shot = 1;
+                            } else {
+                                p2->is_shot = 1;
+                            }
+                        }
+
+                        if (!strcmp(msg_type, "UPHP")) {
+                            char item_id_str[2];
+                            strncpy(item_id_str, &buff[5], 1);
+                            item_id_str[1] = '\0';
+                            int item_id = atoi(item_id_str);
+                            if (i == 0) {
+                                p1->hp++;
+                            } else {
+                                p2->hp++;
+                            }
+                            items_state[item_id] = '0';
+                        }
+
+                        if (!strcmp(msg_type, "UPAT")) {
+                            char item_id_str[2];
+                            strncpy(item_id_str, &buff[5], 1);
+                            item_id_str[1] = '\0';
+                            int item_id = atoi(item_id_str);
+                            if (i == 0) {
+                                p1->power_elapsed = MAX_POWER_TIME;
+                                power_clock1 = time(0);
+                            } else {
+                                p2->power_elapsed = MAX_POWER_TIME;
+                                power_clock2 = time(0);
+                            }
+                            items_state[item_id] = '0';
+                        }
+
+
                         char state[MAX_MSG_LEN];
-                        sprintf(state, "P1: %d - P2: %d", h1, h2);
+
+                        sprintf(state, "STAT:P1:%d,%d,%d-%d-%d-%d|P2:%d,%d,%d-%d-%d-%d|ITEMS:%s|TIME:%d", 
+                                p1->coord[0], p1->coord[1], p1->coord[2], p1->hp, p1->is_shot, p1->power_elapsed,
+                                p2->coord[0], p2->coord[1], p2->coord[2], p2->hp, p2->is_shot, p2->power_elapsed,
+                                items_state,
+                                time_elapsed);
+
                         printf("%s\n", state);
                         send_clients_msg(pfds[0].fd, pfds[1].fd, state);
                         
-                        if (h1 == 0) {
-                            send_client_msg(pfds[1].fd, "YOU WON");
-                            send_client_msg(pfds[0].fd, "YOU LOOSE");
+                        if (p1->hp <= 0) {
+                            send_client_msg(pfds[1].fd, "ENDS:WON");
+                            send_client_msg(pfds[0].fd, "ENDS:LOSE");
                             gameover = 1;
                         }
-                        if (h2 == 0) {
-                            send_client_msg(pfds[0].fd, "YOU WON");
-                            send_client_msg(pfds[1].fd, "YOU LOOSE");
+                        if (p2->hp <= 0) {
+                            send_client_msg(pfds[0].fd, "ENDS:WON");
+                            send_client_msg(pfds[1].fd, "ENDS:LOSE");
                             gameover = 1;
                         }
                         
+                        reset_stats(p1, p2);
                         
                         bzero(buff, MAX_MSG_LEN);
 					}
@@ -250,7 +391,7 @@ void *client_connection_handler(void *cli_sockfd) {
 
 	}
     if (read_len <= 0) 
-        printf("Player %d is disconnected.\n", socket);
+        printf("[DEBUG]Player %d is disconnected.\n", socket);
 
 	pthread_exit(NULL);
 	return 0;
@@ -258,26 +399,26 @@ void *client_connection_handler(void *cli_sockfd) {
 
 int matching_request_handler(char *message, int msg_len, int socket) {
     
-    char msg_type[4];
+    char msg_type[5];
 
-    strncpy(msg_type, &message[0], 3);
-    msg_type[3] = '\0';
+    strncpy(msg_type, &message[0], 4);
+    msg_type[4] = '\0';
 
    
-    if (!strcmp(msg_type, "CCL")) {
-        printf("Player cancel waiting opponent.\n");
+    if (!strcmp(msg_type, "DROP")) {
+        printf("[DEBUG]Player %d cancel waiting opponent.\n", socket);
 
         pthread_mutex_lock(&mutex_room_queue);
         int roomId = s_delete_node(roomQueue, socket);
         pthread_mutex_unlock(&mutex_room_queue);
 
-        printf("Delete room: %d.\n", roomId);
+        printf("[DEBUG]Delete room: %d.\n", roomId);
 
-        send_client_msg(socket, "OK");
+        send_client_msg(socket, "DROP:OK");
 
         return 0;
 
-    } else if (!strcmp(msg_type, "CRT")) {
+    } else if (!strcmp(msg_type, "CREA")) {
 
         srand(time(NULL));   
         int room_id = rand() % 1000;  
@@ -286,16 +427,17 @@ int matching_request_handler(char *message, int msg_len, int socket) {
         s_enqueue(roomQueue, room_id, socket);
         pthread_mutex_unlock(&mutex_room_queue);
 
-        printf("Created Room: %d for player %d\n", room_id, socket);
-
-        send_client_msg(socket, "OK");
+        printf("[DEBUG]Created Room: %d for player %d\n", room_id, socket);
+        char send_message[MAX_MSG_LEN];
+        sprintf(send_message, "CREA:%d", room_id);
+        send_client_msg(socket, send_message);
 
         return 0;
 
-    } else if (!strcmp(msg_type, "JOI")) {
+    } else if (!strcmp(msg_type, "JOIN")) {
 
         char room_id_str[4];
-        strncpy(room_id_str, &message[4], 3);
+        strncpy(room_id_str, &message[5], 3);
         room_id_str[3] = '\0';
         int room_id = atoi(room_id_str);
 
@@ -305,7 +447,7 @@ int matching_request_handler(char *message, int msg_len, int socket) {
 
         struct Room* temp = roomQueue->front;
         int is_room_available = 0;
-
+       
         while (temp != NULL) {
             // printf("Room: %d", temp->key);
             if (temp->key == room_id && temp->is_ready == 0 & temp->is_start == 0) {
@@ -320,22 +462,27 @@ int matching_request_handler(char *message, int msg_len, int socket) {
         pthread_mutex_unlock(&mutex_room_queue);
 
         if (is_room_available) {
-            printf("Match with room %d successfully.\n", room_id);
-            send_clients_msg(client1, socket, "MAT");
+            printf("[DEBUG]Match client %d and client %d with room %d successfully.\n", client1, socket, room_id);
+            char send_message[MAX_MSG_LEN];
+            sprintf(send_message, "MATC:%d-2", room_id);
+            send_client_msg(socket, send_message);
+            
+            sprintf(send_message, "MATC:%d-1", room_id);
+            send_client_msg(client1, send_message);
         } else {
-            printf("No available room with id: %d.\n", room_id);
-            send_client_msg(socket, "FIL");
+            printf("[DEBUG]No available room with id: %d.\n", room_id);
+            send_client_msg(socket, "JOIN:FAIL");
         }
 
         return 0;
 
-    } else if (!strcmp(msg_type, "LIS")) {
+    } else if (!strcmp(msg_type, "LIST")) {
 
         pthread_mutex_lock(&mutex_room_queue);
         struct Room* temp = roomQueue->front;
 
         if (temp == NULL) 
-            printf("No room available.\n");
+            printf("[DEBUG]No room available.\n");
 
         while (temp != NULL) {
             printf("  Room: %d\n- Client1: %d - Client2: %d\n- Ready: %d - Start: %d.\n", temp->key, temp->client1, temp->client2, temp->is_ready, temp->is_start);
@@ -343,19 +490,19 @@ int matching_request_handler(char *message, int msg_len, int socket) {
         }
         pthread_mutex_unlock(&mutex_room_queue);
 
-        send_client_msg(socket, "OK");
+        send_client_msg(socket, "LIST:OK");
         
         return 0;
 
-    } else if (!strcmp(msg_type, "STA")) {
+    } else if (!strcmp(msg_type, "PLAY")) {
 
-        send_client_msg(socket, "GAME START");
+        send_client_msg(socket, "PLAY:OK");
 
         return 1;
 
     } else {
 
-        send_client_msg(socket, "INV");
+        send_client_msg(socket, "IVLD");
 
         return 0;
     }
